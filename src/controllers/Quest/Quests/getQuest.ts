504c3415ebfb3e2debf58e2543ce4dfce4bbbe6c
ITMsJarvis <ck8824deb@gmail.com>
2025-04-21 19:03:36 +0530

    fix: the quest by Id thingy to hold off the hasApplied and the isFavorite

--- src/controllers/Quest/Quests/getQuest.ts
+++ src/controllers/Quest/Quests/getQuest.ts
@@ -4,18 +4,115 @@ import { errors, handleResponse } from '../../../utils/responseCodec'
 import Joi from 'joi'
 import { sendErrorToDiscord } from '../../../config/discord/errorDiscord'
 import { QUESTS } from '../../../models/Quest/quest.model'
+import { Types } from 'mongoose'
+
 
 export const getQuest = async (req: Request, res: Response) => {
     try {
-        const validationError: Joi.ValidationError | undefined = validateQuestId(req.params);
+       const validationError: Joi.ValidationError | undefined = validateQuestId(req.params);
         if (validationError) {
             return handleResponse(res, 400, errors.validation, validationError.details);
         }
-        const { questId } = req.params
-        const questdetails = await QUESTS.findById(questId).populate('user', 'username photo name')
-        return handleResponse(res, 200, { questdetails })
-    } catch (error) {
-        sendErrorToDiscord("GET:get-comment", error)
-        return handleResponse(res, 500, errors.catch_error)
+
+        const { questId } = req.params;
+        const userId = res.locals.userId;
+        const questObjectId = new Types.ObjectId(questId);
+        const userObjectId = new Types.ObjectId(userId);
+        const pipeline: any[] = [
+            { $match: { _id: questObjectId } },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "user",
+                    foreignField: "_id",
+                    as: "user",
+                    pipeline: [
+                        {
+                            $project: {
+                                _id: 1,
+                                username: 1,
+                                photo: 1,
+                                name : 1,
+                            }
+                        }
+                    ]
+                }
+            },
+            { $unwind: "$user" },
+            {
+                $lookup: {
+                    from: "questfavs",
+                    let: { questId: "$_id" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: {
+                                    $and: [
+                                        { $eq: ["$quest", "$$questId"] },
+                                        { $eq: ["$user", userObjectId] }
+                                    ]
+                                }
+                            }
+                        }
+                    ],
+                    as: "userFavorites"
+                }
+            },
+            {
+                $addFields: {
+                    isFavorite: { $gt: [{ $size: "$userFavorites" }, 0] }
+                }
+            },
+            { $unset: "userFavorites" },
+            {
+                $addFields: {
+                    isOwner: { $eq: ["$user._id", userObjectId] }
+                }
+            },
+            {
+                $lookup: {
+                    from: "questapplications",
+                    let: { questId: "$_id", isOwner: "$isOwner" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: {
+                                    $and: [
+                                        { $eq: ["$quest", "$$questId"] },
+                                        { $eq: ["$user", userObjectId] }
+                                    ]
+                                }
+                            }
+                        }
+                    ],
+                    as: "userApplications"
+                }
+            },
+            {
+                $addFields: {
+                    hasApplied: {
+                        $cond: {
+                            if: { $eq: ["$isOwner", false] },
+                            then: { $gt: [{ $size: "$userApplications" }, 0] },
+                            else: false
+                        }
+                    }
+                }
+            },
+            { $unset: ["userApplications", "isOwner"] }
+        ];
+
+        const result = await QUESTS.aggregate(pipeline);
+
+        if (!result.length) {
+            return handleResponse(res, 404, errors.quest_not_found);
+        }
+
+        return handleResponse(res, 200, { questdetails: result[0] });
+
+    } catch (err) {
+        console.error(err);
+        sendErrorToDiscord("GET:aggregated-quest", err);
+        return handleResponse(res, 500, errors.catch_error);
     }
-}
+};
