2d9532123da442e0a4bb140674bd73d4f9b93aaf
ITMsJarvis <ck8824deb@gmail.com>
2025-05-16 13:42:17 +0530

    fix:delete-account controllers on the router level

--- src/routes/deactivateDelete.routes.ts
+++ src/routes/deactivateDelete.routes.ts
@@ -1,5 +1,6 @@
 import express, { Router } from "express";
 import { deactivateAccount } from "../controllers/Deativate-Delete/deactivateAccount";
+import { deleteAccount } from "../controllers/Deativate-Delete/deleteAccount";
 
 
 export const deactivationDeleteRoutes: Router = express.Router();
@@ -7,5 +8,5 @@ export const deactivationDeleteRoutes: Router = express.Router();
 
 deactivationDeleteRoutes.post("/deactivate-account", deactivateAccount)
 
-deactivationDeleteRoutes.delete("/delete-account", deactivateAccount)
+deactivationDeleteRoutes.delete("/delete-account", deleteAccount)
 
