8cf7abd3a99b6210ac39644e45fe5bff3e881768
ITMsJarvis <ck8824deb@gmail.com>
2025-05-28 12:53:12 +0530

    fix: rebuilding issues

--- src/routes/normal.routes.ts
+++ src/routes/normal.routes.ts
@@ -1,6 +1,6 @@
 import { authRoutes } from "./auth/auth.routes";
 import { onboardingRoutes } from "./onboarding/onboarding.routes";
-import { flickRoutes } from "./flicks/unprotected-flicks.routes";
+// import { flickRoutes } from "./flicks/unprotected-flicks.routes";
 import { forgetPasswordRoutes } from "./forget-password/forgetPassword.routes";
 import { commonRoutes } from "./common/common.routes";
 import { unprotectedProfileRoutes } from "./profile/unprotectedProfile.routes";
@@ -11,7 +11,7 @@ const routes = [
     authRoutes,
     forgetPasswordRoutes,
     onboardingRoutes,
-    flickRoutes,
+    // flickRoutes,
     commonRoutes,
     unprotectedProfileRoutes,
     songRoutes
