23fe681e46cd3a0a6d68058df28c2b896bd0fa2e
ITMsJarvis <ck8824deb@gmail.com>
2025-07-21 05:36:10 +0530

    fix: emailing issues for change in the SMTP from zoho--> gmail

--- src/config/nodemailer/nodemailer.config.ts
+++ src/config/nodemailer/nodemailer.config.ts
@@ -1,7 +1,7 @@
 import { config } from "../generalconfig";
 
 interface nodeMailerConf {
-    host: string, 
+    host: string,
     port: number,
     secure: boolean,
     auth: {
@@ -11,9 +11,9 @@ interface nodeMailerConf {
 }
 
 
- export let nodemailerConfig: nodeMailerConf = {
-     host: 'smtp.zoho.com',
-     port: 465,
-     secure: true,
+export let nodemailerConfig: nodeMailerConf = {
+    host: 'smtp.gmail.com',
+    port: 465, // use 465 for SSL or 587 for STARTTLS
+    secure: true, // true for port 465, false for 587
     auth: config.EMAILCONFIG,
 }
