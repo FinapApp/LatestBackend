564b879d100cc76a50ee53c72a3217399265adc0
ITMsJarvis <ck8824deb@gmail.com>
2025-08-05 19:25:15 +0530

    fix: the path name for the flicks

--- src/controllers/Flicks/createPresigedURLFlick.ts
+++ src/controllers/Flicks/createPresigedURLFlick.ts
@@ -22,9 +22,9 @@ export const createPresignedURLFlick = async (req: Request, res: Response) => {
             if (mediaFiles) {
                 mediaSignedURL = await Promise.all(mediaFiles.map((x: { fileName: string, fileType: string }) => generateSignedURL(`${user}/flick/${flickId}/videos/${x.fileName}`, x.fileType)));
             }
-            thumbnailSignedUrl = await generateSignedURL(`/user/${user}/flick/${flickId}/thumbnail.jpeg`, "image/jpeg");
+            thumbnailSignedUrl = await generateSignedURL(`user/${user}/flick/${flickId}/thumbnail.jpeg`, "image/jpeg");
             if (audioName) {
-                const audioPath = `/user/${user}/flick/audio/${flickId}/${audioName}`;
+                const audioPath = `user/${user}/flick/audio/${flickId}/${audioName}`;
                 audioSignedURL = await generateSignedURL(audioPath, audioFileType);
             }
             return handleResponse(res, 200, {
